2d55458f9aa0812abb00e93c0fb2ec2201f6275f
Valeri Hristov <valerimagic@gmail.com>
2022-12-12 22:53:37 +0200

    update

--- Java Basic/New beginning/src/BonusScore.java
+++ Java Basic/New beginning/src/BonusScore.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class BonusScore {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        int number = Integer.parseInt(input.nextLine());
+
     }
 }
