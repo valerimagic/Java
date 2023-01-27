ea1ea0203f48fffd0dda31974489788b281a75e7
Valeri Hristov <valerimagic@gmail.com>
2023-01-27 23:07:18 +0200

    Update Main.java

--- Java Basic/New beginning/src/Main.java
+++ Java Basic/New beginning/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("    Hello Valeri ");
-        System.out.print("    Spasov  ");
-        System.out.print("    Hristov  ");
-        System.out.print("    from   Shumen, ");
-        System.out.print("    Bulgaria  ");
+        System.out.print("      Hello Valeri ");
+        System.out.print("      Spasov  ");
+        System.out.print("      Hristov  ");
+        System.out.print("      from   Shumen, ");
+        System.out.print("      Bulgaria  ");
         
     }
 }
