67d388ccd757cd698fa2b3b062d0094156d4c0a3
Valeri Hristov <valerimagic@gmail.com>
2023-02-13 18:54:52 +0200

    add exe Subsequence

---
+++ Exercise/src/ExerciseArray/Subsequence.java
@@ -0,0 +1,27 @@
+package ExerciseArray;
+
+import java.util.Scanner;
+
+public class Subsequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        var numbers = new int[]{1, 1, 1, 2, 3, 1, 1, 1, 2, 1, 1, 2, 3, 4, 1, 1, 2};
+
+        int longestLength = 0;
+        int currentLength = 1;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] < numbers[i + 1]) {
+                currentLength++;
+            } else {
+                if (currentLength > longestLength) {
+                    longestLength = currentLength;
+                }
+                currentLength = 1;
+            }
+        }
+
+        System.out.println(longestLength);
+    }
+}
