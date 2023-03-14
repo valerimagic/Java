332ec6f147d0bd6e91677d7e29f32dab4d64ee0c
Valeri Hristov <valerimagic@gmail.com>
2023-03-14 23:00:39 +0200

    Update Student.java

--- Exercise/src/ExerciseArray/Student.java
+++ Exercise/src/ExerciseArray/Student.java
@@ -1,5 +1,9 @@
 package ExerciseArray;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Student {
     String firsName;
     private String lastName;
@@ -8,7 +12,7 @@ public class Student {
 
     public Student(String firsName, String lastName, int age, String citi) {
         this.firsName = firsName;
-        this.lastName = lastName;
+        this.lastName = lastName, firstname;
         this.age = age;
         this.citi = citi;
     }
@@ -23,6 +27,28 @@ public class Student {
 
     public void SetFirstName(String firsName) {
         this.firsName = firstName;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        List<Student> studentList = new ArrayList<>();
+        while (!input.equals("end")) {
+
+            String[] studentsArr = input.split(" ");
+            Student student = new Student(studentsArr[0], studentsArr[1], studentsArr[2], studentsArr[3]);
+            studentList.add(student);
+
+            input = scanner.nextLine();
+        }
+
+        String city = scanner.nextLine();
+        for (Student item : studentList){
+            if (item.getCity().equals(city)){
+                System.out.println(item.getFirstName() + " " + item.getSecondName() + " is " + item.getAge() + " years old");
+            }
+        }
     }
 
 }
