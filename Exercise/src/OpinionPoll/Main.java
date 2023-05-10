62ddc27764b05dedd827b24a6aa862e156804ed0
Valeri Hristov <valerimagic@gmail.com>
2023-05-10 23:47:21 +0300

    Update Main.java

--- Exercise/src/OpinionPoll/Main.java
+++ Exercise/src/OpinionPoll/Main.java
@@ -1,25 +1,23 @@
 package OpinionPoll;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
 
-        List<Person> list = new LinkedList<>();
+        List<Person> list = new ArrayList<>();
 
         for (int i = 0; i < number; i++) {
             String[] input = scanner.nextLine().split(" ");
-
             Person person = new Person(input[0], Integer.parseInt(input[1]));
-
             list.add(person);
-
-
         }
-        //Collections.sort(list);
+
         for (Person person : list) {
             if(person.getAge() > 30){
                 System.out.println(person.getName() + "-" + person.getAge());
