02386938313d40d8d266880d72905884dfe06a8e
Valeri Hristov <valerimagic@gmail.com>
2023-05-11 23:58:10 +0300

    Update Person.java

--- Exercise/src/OpinionPoll/Person.java
+++ Exercise/src/OpinionPoll/Person.java
@@ -10,22 +10,18 @@ public class Person {
     }
 
     public String getName() {
-
         return this.name;
     }
 
     public void setName(String name) {
-
         this.name = name;
     }
 
     public int getAge() {
-
         return this.age;
     }
 
     public void setAge(int age) {
-
         this.age = age;
     }
 }
